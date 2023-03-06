cfdc750c3aa60b56ac412890d5611375e784b634
Viktoriia Moskalenko <vika01mos@gmail.com>
2023-03-06 16:19:28 +0200

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,13 +23,7 @@ export default function Home() {
           color="#f5f5f5"
           fontSize="24px"
         >
-          Save your contacts - this is a convenient application that is always
-          at hand, with which you no longer need to look for such an important
-          contact in your notes, now all contacts are on your phone. an
-          intuitive interface will help you add new contacts very quickly, and a
-          convenient filter will help you sort contacts by name and you will no
-          longer have to scroll through endless lists of contacts.
-          <div>Save your contacts - welcome and have a nice day!</div>
+          <div>Have a nice day!</div>
         </Text>
       </Box>
     </HelmetProvider>
